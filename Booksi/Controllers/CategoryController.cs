28b1974ec086c86124d718d747c3c7d5286edd77
Artur Tryc <tryc.artur@gmail.com>
2024-11-17 18:25:15 +0100

    Category HttpPost

--- Booksi/Controllers/CategoryController.cs
+++ Booksi/Controllers/CategoryController.cs
@@ -19,4 +19,10 @@ public class CategoryController : Controller
         return View();
     }
     
+    [HttpPost]
+    public IActionResult Create(Category category){
+        _db.Categories.Add(category);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 }
