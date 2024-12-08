1747bb3173cdd7e83f36e9c63ed966cb2da13335
Artur Tryc <tryc.artur@gmail.com>
2024-12-08 20:47:01 +0100

    Changes - dotnet run work, docker compose failed

--- Booksi/Controllers/CategoryController.cs
+++ Booksi/Controllers/CategoryController.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Booksi.Models;
-using Booksi.DataAccess;
-
 public class CategoryController : Controller
 {
     private readonly ILogger<CategoryController> _logger;
