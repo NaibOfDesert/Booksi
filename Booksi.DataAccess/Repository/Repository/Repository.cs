0dc957e6d687f9bc93cef2e7fcc40d69bd6727d5
Artur Tryc <tryc.artur@gmail.com>
2024-12-30 17:25:22 +0100

    Repository Changes

--- Booksi.DataAccess/Repository/Repository/Repository.cs
+++ Booksi.DataAccess/Repository/Repository/Repository.cs
@@ -1,5 +1,24 @@
-using Booksi.DataAccess.Repository.IRepository
-public class Repository : IRepository
+using Booksi.DataAccess.Repository.IRepository;
+public class Repository<T> : IRepository<T> where T : class
 {
+    // Dependency Injection
 
+    T Get(Expression<Func<T, bool>> filter){
+        return Get(filter, null);
+    }
+    IEnumerable <T> GetAll(){
+        return GetAll(null);
+    }
+    void Add(T item){
+
+    }
+    void Update(T item){
+
+    }
+    void Delete(T item){
+
+    }
+    void DeleteMany(IEnumerable<T> items){
+        
+    }
 }
