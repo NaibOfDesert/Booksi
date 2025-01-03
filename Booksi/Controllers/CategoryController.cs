f185ecf30d6b2e31d14925874e3602637188ed19
Artur Tryc <tryc.artur@gmail.com>
2025-01-03 15:39:31 +0100

    CategoryRepository from UnitOfWork change - all work.

--- Booksi/Controllers/CategoryController.cs
+++ Booksi/Controllers/CategoryController.cs
@@ -10,16 +10,14 @@ public class CategoryController : Controller
 {
     private readonly ILogger<CategoryController> _logger;
     private readonly IUnitOfWork _unitOfWork;
-    private readonly ICategoryRepository _categoryRepository;
-    public CategoryController(ILogger<CategoryController> logger, ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+    public CategoryController(ILogger<CategoryController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
         _unitOfWork = unitOfWork;
-        _categoryRepository = categoryRepository;
     }
 
     public IActionResult Index(){
-        IEnumerable<Category> categories = _categoryRepository.GetAll().ToList();
+        IEnumerable<Category> categories = _unitOfWork.categoryRepository.GetAll().ToList();
         return View(categories);
     }
     
@@ -34,7 +32,7 @@ public class CategoryController : Controller
             ModelState.AddModelError("", "Name cannot be a number");
         }
         if(ModelState.IsValid){
-            _categoryRepository.Add(category);
+            _unitOfWork.categoryRepository.Add(category);
             _unitOfWork.Save();
             TempData["Success"] = "Category Succesfully Created"; 
             return RedirectToAction("Index");
@@ -46,7 +44,7 @@ public class CategoryController : Controller
         if(id == null || id == 0){
             return NotFound();
         }
-        Category? category = _categoryRepository.Get(x => x.Id == id);
+        Category? category = _unitOfWork.categoryRepository.Get(x => x.Id == id);
         if(category == null){
             return NotFound();
         }
@@ -60,7 +58,7 @@ public class CategoryController : Controller
             ModelState.AddModelError("", "Name cannot be a number");
         }
         if(ModelState.IsValid){
-            _categoryRepository.Update(category);
+            _unitOfWork.categoryRepository.Update(category);
             _unitOfWork.Save();
             TempData["Success"] = "Category Succesfully Edited"; 
 
@@ -75,11 +73,11 @@ public class CategoryController : Controller
         if(id == null || id ==0){
             return NotFound();
         }
-        Category? category= _categoryRepository.Get(x => x.Id == id);
+        Category? category= _unitOfWork.categoryRepository.Get(x => x.Id == id);
         if(category == null){
             return NotFound();
         }
-        _categoryRepository.Delete(category); 
+        _unitOfWork.categoryRepository.Delete(category); 
         _unitOfWork.Save();
         TempData["Success"] = "Category Succesfully Deleted"; 
         return RedirectToAction ("Index"); 
