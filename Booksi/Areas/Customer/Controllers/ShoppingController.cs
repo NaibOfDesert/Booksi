d9b51fb93c443f3d6df39d7f9b6f564e4023c2de
Artur Tryc <58524861+NaibOfDesert@users.noreply.github.com>
2025-02-10 23:50:37 +0100

    ShoppingController

---
+++ Booksi/Areas/Customer/Controllers/ShoppingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Booksi.Areas.Customer.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
