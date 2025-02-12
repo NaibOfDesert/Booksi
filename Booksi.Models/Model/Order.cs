1c533153561382f00d81e9a407e5c100510492ea
Artur Tryc <tryc.artur@gmail.com>
2025-02-12 22:04:58 +0100

    Order and Summary

---
+++ Booksi.Models/Model/Order.cs
@@ -0,0 +1,39 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using Booksi.Utility;
+
+namespace Booksi.Models.Model{
+    public class Order{
+        [Key]
+        public int Id { get; set; }
+        public int AppUserId { get; set; }
+        [ForeignKey("AppUserId")]
+        [ValidateNever]
+        public AppUser AppUser { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+
+
+        public double TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Today;
+        public DateTime DeliveryDate { get; set; }
+
+        public string? OrderStatus { get; set; }
+        public bool PaymentStatus { get; set; }
+    }
+}
---
+++ Booksi.Utility/OrderStatus.cs
@@ -0,0 +1,11 @@
+using System.Data;
+
+namespace Booksi.Utility {
+    public static class OrderStatus {
+        public const string OrderStatusToPay = "ToPay"; 
+        public const string OrderStatusInPrepare = "InPreparation";
+        public const string OrderStatusSended = "Sended";
+        public const string OrderStatusDelivered = "Delivered";
+
+    }
+}
