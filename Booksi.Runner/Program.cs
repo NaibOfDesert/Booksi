8972c6b514240be1c06d8030bea55a6f267bf805
Artur Tryc <tryc.artur@gmail.com>
2025-04-22 23:54:27 +0200

    Runner update.

--- Booksi.Runner/Program.cs
+++ Booksi.Runner/Program.cs
@@ -9,8 +9,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("");
 
 Menu();
-var input = Console.ReadLine();
-
+string? input = Console.ReadLine();
+Switch(input);
  
 
 void Menu(){
@@ -18,27 +18,55 @@ void Menu(){
     Console.WriteLine("Description: \n"); 
 
     Console.WriteLine("Options: \n" + 
-        "1. Run tests (dotnet run )" + 
-        "2. Docker compose up build" + 
-        "3. Db)");
+        "1. Build" + 
+        "2. Up" + 
+        "3. Run");
 }
 
-void Switch(string inpput) {
-    switch (inpput) {
-        case "1":
-            Console.WriteLine("Test running\n");
-            // start docker try
-            
-            System.Diagnostics.Process.Start("dotnet", "run --project ../Booksi/Booksi.csproj");
-            break;
-        case "2":
-            System.Diagnostics.Process.Start("dotnet", "run --project ../Booksi.PlaywrightRunner/Booksi.PlaywrightRunner.csproj");
-            break;
-        case "3":
-            break;
-        default:
-            break;
+void MenuUp(){
+
+    Console.WriteLine("Description: \n"); 
+
+    Console.WriteLine("Options: \n" + 
+        "1. Db Add Migration" + 
+        "2. Up Update" + 
+        "3. Docker Compose");
+}
+
+
+void Switch(string? input1, string input2 = "0") {
+    if(input != null) {
+        switch (input1) {
+            case "1":
+                Console.WriteLine("Build\n");
+                System.Diagnostics.Process.Start("dotnet", "build --project ../Booksi.csproj");
+                break;
+            case "2":
+                Console.WriteLine("Up\n");
+                switch (input2){
+                    case "1":
+                    break;
+                }
+
+                System.Diagnostics.Process.Start("dotnet", "run --project ../Booksi.PlaywrightRunner/Booksi.PlaywrightRunner.csproj");
+                break;
+            case "3":
+                break;
+            default:
+                break;
+        }
+    
     }
+    else {
+        throw new Exception("Value cannot be null");
+    }
+
+}
+
+
+
+void Run() {
+
 }
 
 void Clear() {
