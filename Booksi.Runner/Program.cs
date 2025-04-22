03fa5fd5a528a6a50d6a7182195cd33a335d9f22
Artur Tryc <tryc.artur@gmail.com>
2025-04-22 22:53:36 +0200

    Runner menu to update.

--- Booksi.Runner/Program.cs
+++ Booksi.Runner/Program.cs
@@ -4,4 +4,46 @@ Console.WriteLine("Hello, World!");
 
 
 // Run PlaywrightRunner for Testing.
-System.Diagnostics.Process.Start("dotnet", "run --project ../Booksi.PlaywrightRunner/Booksi.PlaywrightRunner.csproj");
+
+
+Console.WriteLine("");
+
+Menu();
+var input = Console.ReadLine();
+
+ 
+
+void Menu(){
+
+    Console.WriteLine("Description: \n"); 
+
+    Console.WriteLine("Options: \n" + 
+        "1. Run tests (dotnet run )" + 
+        "2. Docker compose up build" + 
+        "3. Db)");
+}
+
+void Switch(string inpput) {
+    switch (inpput) {
+        case "1":
+            Console.WriteLine("Test running\n");
+            // start docker try
+            
+            System.Diagnostics.Process.Start("dotnet", "run --project ../Booksi/Booksi.csproj");
+            break;
+        case "2":
+            System.Diagnostics.Process.Start("dotnet", "run --project ../Booksi.PlaywrightRunner/Booksi.PlaywrightRunner.csproj");
+            break;
+        case "3":
+            break;
+        default:
+            break;
+    }
+}
+
+void Clear() {
+    System.Diagnostics.Process.Start("clear");
+}
+void MenuDb() {
+    Console.WriteLine("");
+}
